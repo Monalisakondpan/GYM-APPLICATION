4dc1e8987e361db241345844adc097f2bdc586b4
Monalisa Kondpan <110601214+Monalisakondpan@users.noreply.github.com>
2024-10-12 22:44:17 +0530

    Project

---
+++ Frontend/src/components/WorkoutSessions.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const WorkoutSessions = () => {
+  return (
+    <section className='workout_session'>
+      <div className='wrapper'>
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Omnis dolorum ducimus ad iusto dolores eveniet rem cumque velit!
+        </p>
+        <img src="/img2.jpg" alt="workout"/>
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Fugit, dolor enim sit soluta ea quae ducimus quidem totam!
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem ipsum dolor, 
+            sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+             Harum perspiciatis ipsa id aut quasi nisi!
+             </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor, 
+            sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+             Harum perspiciatis ipsa id aut quasi nisi!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor, 
+            sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+             Harum perspiciatis ipsa id aut quasi nisi!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor, 
+            sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+             Harum perspiciatis ipsa id aut quasi nisi!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkoutSessions;
